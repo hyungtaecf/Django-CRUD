728ef8da25728f12202531ca4996f0c6b5eed9a8
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-05-22 01:44:07 -0300

    Added Prev/Next Button - Not perfect yet! Should Review

--- crud_ajax/views.py
+++ crud_ajax/views.py
@@ -1,12 +1,59 @@
 from .models import CrudUser
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, View
+from django.template import loader
+from django.views.generic import View #, ListView
+from django.core import serializers
 
-class CrudView(ListView):
-    model = CrudUser
-    template_name = 'crud_ajax/crud.html'
-    context_object_name = 'users'
+
+displayingItemsNumber = 3
+
+# class CrudView(ListView):
+#     model = CrudUser
+#     template_name = 'crud_ajax/crud.html'
+#     context_object_name = 'users'
+
+class CrudView(View):
+    def get(self, request):
+        users = CrudUser.objects.all()[:displayingItemsNumber]
+        # users = CrudUser.objects.all().order_by('id')[1:CrudUser.objects.all().count()-1]
+        template = loader.get_template('crud_ajax/crud.html')
+        context = {
+            'users': users,
+        }
+        return HttpResponse(template.render(context, request))
+
+class RetrieveCrudUser(View):
+    def get(self, request):
+        # Gets via ajax the requested indexes expressed in javascript
+        fromIndex = int(request.GET.get('fromIndex', None))
+        toIndex = int(request.GET.get('toIndex', None))
+
+        # Checking pagination position
+        first_page = False
+        last_page = False
+        if fromIndex > CrudUser.objects.all().count()-1: # Is it going to the First Page?
+            first_page = True
+            fromIndex = 0
+            toIndex = displayingItemsNumber
+        elif fromIndex < 0: # Is it going to the Last Page?
+            last_page = True
+            fromIndex = (CrudUser.objects.all().count() - displayingItemsNumber
+                + CrudUser.objects.all().count()%displayingItemsNumber)
+            toIndex = CrudUser.objects.all().count()
+
+        if toIndex > CrudUser.objects.all().count(): # Is it the Last Page and not full?
+            toIndex = CrudUser.objects.all().count()
+
+        users = serializers.serialize("json",CrudUser.objects.all().order_by('id')[fromIndex:toIndex])
+        data = {
+            'users': users,
+            'fromIndex': fromIndex,
+            'toIndex': toIndex,
+            'first_page' : first_page,
+            'last_page' : last_page
+        }
+        return JsonResponse(data)
 
 class CreateCrudUser(View):
     def  get(self, request):
--- django_crud/urls.py
+++ django_crud/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('ajax/crud/create/',  views.CreateCrudUser.as_view(), name='crud_ajax_create'),
     path('ajax/crud/update/',  views.UpdateCrudUser.as_view(), name='crud_ajax_update'),
     path('ajax/crud/delete/',  views.DeleteCrudUser.as_view(), name='crud_ajax_delete'),
+    path('ajax/crud/retrieve/',  views.RetrieveCrudUser.as_view(), name='crud_ajax_retrieve'),
 ]
